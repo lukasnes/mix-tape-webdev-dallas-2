75949b48644d3243de6de5f2fa981beb57796859
Sam Bunn <sammbunnn@gmail.com>
2023-10-16 12:20:14 -0500

    playlist header created

--- src/Pages/Playlist.jsx
+++ src/Pages/Playlist.jsx
@@ -3,7 +3,35 @@ import { useLoaderData } from "react-router-dom";
 const Playlist = () => {
   let playlist = useLoaderData();
 
-  return <></>;
+  const playlistRow = () => {
+
+    return 
+  }
+
+  return  <>
+            <header>
+
+              <div id='tapeMain'>
+                <div id='window'>
+                  <button class="windowButtons" id='topButton'> Top </button>
+                  <button class="windowButtons" id='loginButton'> Login </button>
+                  <button class="windowButtons" id='friendsButton'> Friends </button>
+                </div>
+                <div id='tapeBottom'>
+                  <div></div>
+                </div>
+              </div>
+
+            </header>
+
+            <main>
+              <div id='playlistList'>
+                {playlistRow}
+              </div>
+            </main>
+
+            <footer></footer>
+          </>;
 };
 
 export default Playlist;
