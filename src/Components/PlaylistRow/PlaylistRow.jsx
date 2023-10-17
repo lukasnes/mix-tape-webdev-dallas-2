aaf5cefdbf6ae6d79d942e65cd8f760c5a141d23
Sam Bunn <sammbunnn@gmail.com>
2023-10-17 12:01:08 -0500

    create playlist added to playlist page, edit and delete added to playlist row

--- src/Components/PlaylistRow/PlaylistRow.jsx
+++ src/Components/PlaylistRow/PlaylistRow.jsx
@@ -8,18 +8,22 @@ const PlaylistRow = ({pl}) => {
 
     const deletePlaylist = async () => {
         const res = await axios.post("/api/deleteplaylist")
+        console.log(res.data)
+
       }
 
-    const editPlaylist = () => {
-
+    const editPlaylist = async () => {
+        const res = await axios.post('/api/playlists')
+        let playlistId = res.data.playlistId
+        Navigate(`/playlist/${playlistId}`)
     }
 
     return (
-        <div className="displayRow" onClick={(e) => editPlaylist}>
+        <div className="displayRow" >
             <div id='timeStampDiv'>
                 <p id='timeStamp'> {pl.createdAt} </p> 
             </div>
-            <div id='playlistNameDiv'>
+            <div id='playlistNameDiv' onClick={(e) => editPlaylist}>
                 <p id='playlistName'> {pl.name} </p>
             </div>
             <div id='deleteButtonDiv'>
