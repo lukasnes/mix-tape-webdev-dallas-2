184ad2dcf198c168e29f5bc79eed7023b5dfddba
Sam Bunn <sammbunnn@gmail.com>
2023-10-17 15:58:15 -0500

    login modal created

---
+++ src/Components/Modal/Login/Login.jsx
@@ -0,0 +1,54 @@
+import {Modal} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+function LoginModal({show, setShow}) {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = async () => {
+        event.preventDefault()
+        const res = await axios.post('/api/login', {email, password})
+        // todo need response
+    }
+
+  return (
+    <div
+      className="modal show"
+      style={ open ? { display: 'block', position: 'initial' } : {display: 'none'}}
+    >
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Please Login</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <form id='loginForm' onSubmit={handleLogin} >
+            <label for="email" />
+            <input id="email" 
+                type="email" 
+                value={email} 
+                onChange={(e)=>setEmail(e.target.value)} 
+                placeholder='Please enter email'
+                required />
+            <label for="password" />
+            <input id="password"
+                type="text"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                placeholder='please enter a password'
+                required />
+            </form>    
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <input type='submit' variant="primary">Submit</input>
+        </Modal.Footer>
+      </Modal.Dialog>
+    </div>
+  );
+}
+
+export default LoginModal;
