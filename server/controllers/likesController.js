c945c1689d772c1b72f36213e2ff7ab875e487e9
Joe Aguado <aguado.joe@gmail.com>
2023-10-24 11:33:02 -0500

    playlist controller updates -- J

--- server/controllers/likesController.js
+++ server/controllers/likesController.js
@@ -8,7 +8,30 @@ const getTopLiked = async (req, res) => {
 const getAllPlaylists = await Playlist.findAll()
 res.status(200).json(getAllPlaylists)
 
+const topPlaylists = await Playlist.findAll({
+    include: {
+        model: Likes,
+    },
+    order: [
+        [{ }]
+    ]
+})
 
+// const rankedPlaylists = await Playlist.findAll({
+//     attributes: {
+//       include: [
+//         [sequelize.fn('COUNT', sequelize.col('likes.likesId')), 'likeCount'],
+//       ],
+//     },
+//     include: {
+//       model: Likes,
+//       attributes: [],
+//     },
+//     group: ['Playlist.playlistId'],
+//     order: [[sequelize.literal('likeCount'), 'DESC']],
+//   });
+
+//   res.json(rankedPlaylists)
 
 }
 
