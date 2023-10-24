b4879efdb0d11b8541ce15afd461ad13f4a57b74
Tara Mehryar <tara.mehryar@gmail.com>
2023-10-23 22:04:50 -0500

    Update Add Song Component

--- src/Components/Modal/AddSong/AddSong.jsx
+++ src/Components/Modal/AddSong/AddSong.jsx
@@ -1,86 +1,115 @@
-import React from "react";
-import ReactModal from "react-dom";
-// import Modal from "react-modal";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import "../AddSong/AddSong.css"
-import debounce from 'lodash/debounce';
+import axios from 'axios';
+import '../AddSong/AddSong.css';
 
 function AddSong() {
     const [show, setShow] = useState(false);
     const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(false);
-  
-    const handleClose = () => setShow(false);
+    const [results, setResults] = useState([]);
+    const [selectedSong, setSelectedSong] = useState(null);
+
+    const handleClose = () => {
+        setShow(false);
+        setSelectedSong(null);
+    };
     const handleShow = () => setShow(true);
 
-    // const handleChange = () => {
-    //   const { value: nextValue } = event.target;
-    //   debouncedSave(nextValue);
-    // };
-  
-    // const searchSong = async () => {
-    //   const response = await fetch('' + searchTerm)
-    //   const data = await response.json();
-    //   return data;
-    // };
-
     useEffect(() => {
-      // search the api
+        async function fetchData() {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    `https://cors-anywhere.herokuapp.com/api.deezer.com/search?q=${search}`
+                );
 
-      async function fetchData(){
-        setLoading(true)
+                if (response.status === 200) {
+                    setResults(response.data.data);
+                } else {
+                    // Handle errors here
+                }
+            } catch (error) {
+                // Handle network or parsing errors
+            } finally {
+                setLoading(false);
+            }
+        }
 
-        const data = await fetch(
-          `https://api.deezer.com/search/track?q=${search}`
-        ).then((res) => res.json());
-        setLoading(false);
-      }
-
-      fetchData();
+        fetchData();
     }, [search]);
-  
+
+    const handleSongSelect = (song, e) => {
+        e.preventDefault(); // Prevent the default form submission behavior
+        setSelectedSong(song);
+    };
+
     return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Add New Song
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-  
-          <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Songs</Modal.Title>
-          </Modal.Header>
-  
-          <Modal.Body>
-            <Form>
-              <Form.Label>Search Song</Form.Label>
-              <Form.Control
-              type="song"
-              placeholder="Enter song here"
-              onChange={(e) => setSearch(e.target.value)}
-              autoFocus
-              />
-              <Form.Label>Results</Form.Label>
-              <Form.Control as="results" row={10}/>
-            </Form>
-          </Modal.Body>
-  
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Add New Song
             </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Add
-            </Button>
-          </Modal.Footer>
-          </Modal.Dialog>
-        </Modal>
-      </>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Dialog>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add Songs</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <Form>
+                            <Form.Label>Search Song</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter song here"
+                                onChange={(e) => setSearch(e.target.value)}
+                                autoFocus
+                            />
+                            <Form.Label>Results</Form.Label>
+                            {results && results.length > 0 ? (
+                                <ul>
+                                    {results.map((song) => (
+                                        <li key={song.id}>
+                                            <button
+                                                onClick={(e) => handleSongSelect(song, e)}
+                                            >
+                                                {song.title}
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p>No results found.</p>
+                            )}
+                        </Form>
+
+                        {selectedSong && (
+                            <div>
+                                <h3>{selectedSong.title}</h3>
+                                <p>{selectedSong.album.title}</p>
+                                <img
+                                    src={selectedSong.album.cover_medium}
+                                    alt={selectedSong.title}
+                                />
+                            </div>
+                        )}
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Add
+                        </Button>
+                    </Modal.Footer>
+                </Modal.Dialog>
+            </Modal>
+        </>
     );
-  }
+}
 
 export default AddSong;
+
