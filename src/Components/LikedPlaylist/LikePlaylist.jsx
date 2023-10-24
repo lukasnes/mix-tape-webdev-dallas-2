2fcfae741d5e817a48e62b077a1774b2b0f0753d
Giovanni Decenvirale <giovo_adv@hotmail.com>
2023-10-24 14:56:23 -0500

    FriendList Updated

---
+++ src/Components/LikedPlaylist/LikePlaylist.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import PlaylistRow from "../PlaylistRow/PlaylistRow";
+
+const LikePlaylist = () => {
+  const [likedPlaylist, setLikedPlaylist] = useState([]);
+  const playlistData = likedPlaylist.map(pl => <PlaylistRow pl={pl}/> )
+
+  useEffect(() => {
+    getLikedPlaylist();
+  }, []);
+
+  const getLikedPlaylist = async () => {
+    const res = await axios.get("/api/allmyliked");
+    setLikedPlaylist(res.data);
+  };
+
+  return (
+    <div>
+      {playlistData}
+    </div>
+  );
+};
+
+export default LikePlaylist;
