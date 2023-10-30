a6e03aa24ef820c8cc0645872d93df3b2070ba02
Lukas Nesittka <lukasnesittka25@gmail.com>
2023-10-30 09:25:55 -0500

    Small fixes

--- src/Pages/Songs/Songs.jsx
+++ src/Pages/Songs/Songs.jsx
@@ -4,7 +4,6 @@ import SongsRow from "../../Components/SongsRow/SongsRow";
 import { useState } from "react";
 import "../Songs/Songs.css";
 // import AddSong from "../../Components/Modal/AddSong/AddSong.jsx";
-import SongsModal from "./SongsModal";
 import SearchBar from "../../Components/Modal/AddSong/AddSong.jsx";
 import AddSong from "../../Components/Modal/AddSong/AddSong.jsx";
 import { useNavigate } from "react-router-dom";
