988d6fcca4fcb2abbd81e6c03e1993badc43b7aa
Sam Bunn <sammbunnn@gmail.com>
2023-10-31 11:06:17 -0500

    styled the liked pages

--- src/Components/Friend/Friends.jsx
+++ src/Components/Friend/Friends.jsx
@@ -1,27 +1,26 @@
 import "../Friend/Friend.css";
-import LikePlaylist from "../LikedPlaylist/LikePlaylist";
+// import LikePlaylist from "../LikedPlaylist/LikePlaylist";
 
 
 const Friends = ({ friend, index, setPageState, setFriendId, setLoadingState }) => {
-  console.log(friend)
+  // console.log(friend)
   return (
-    <div>
-      <div id='friendsWrapper'>
-        <div className="friendsContainer" key={index}>
-          <div
+        <div key={index}>
+          
+          <div id="userId">{friend.userId}</div>
+          
+
+          <div id='info'
             onClick={() => {
               setFriendId(friend.userId);
               setLoadingState("friendsPlayList")
               setPageState("loading");
             }}
-            className="friendDisplay"
           >
             {friend.username}
-          </div>
-          <div className="userId">{friend.userId}</div>
         </div>
-      </div>
-    </div>
+          
+        </div>
   );
 };
 
