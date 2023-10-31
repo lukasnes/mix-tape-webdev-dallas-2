1acbaa86f325932a9c6c2c6f3eb306f8f11a0823
Giovanni Decenvirale <giovo_adv@hotmail.com>
2023-10-31 11:07:19 -0500

    follow button on username

--- database/model.js
+++ database/model.js
@@ -1,216 +1,198 @@
-import { 
-    Model, 
-    DataTypes
- } from 'sequelize'
-import util from 'util'
-import connectToDB from './db.js'
+import { Model, DataTypes } from "sequelize";
+import util from "util";
+import connectToDB from "./db.js";
+import { type } from "os";
 
-
-
-export const db = await connectToDB('postgresql:///mixtape')
-
+export const db = await connectToDB("postgresql:///mixtape");
 
 export class User extends Model {
-    [util.inspect.custom](){
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
-
 export class Playlist extends Model {
-    [util.inspect.custom]() {
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 export class Song extends Model {
-     [util.inspect.custom]() {
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 export class FriendList extends Model {
-    [util.inspect.custom](){
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 export class Friend extends Model {
-    [util.inspect.custom](){
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 export class Likes extends Model {
-    [util.inspect.custom](){
-        return this.toJSON()
-    }
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
 }
 
 User.init(
-    {
-        userId: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            unique: true,
-        },
-        username:{
-            type: DataTypes.STRING,
-            unique:true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull:false,
-        }
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
     },
-    {
-        modelName: 'user',
-        sequelize: db,
-    }
-)
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    modelName: "user",
+    sequelize: db,
+  }
+);
 
 Playlist.init(
-    {
-        playlistId: {
-            type: DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey: true,
-            unique:true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull:true,
-        }
+  {
+    playlistId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
     },
-    {
-        modelName: 'playlist',
-        sequelize: db,
-        timestamps: true
-    }
-)
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    modelName: "playlist",
+    sequelize: db,
+    timestamps: true,
+  }
+);
 
 Song.init(
-    {
-        songId:{
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            unique: true
-        },
-        preview: {
-            type: DataTypes.STRING
-        },
-        album:{
-            type: DataTypes.STRING,
-
-        },
-        artist: {
-            type: DataTypes.STRING,
-
-        },
-        name: {
-            type: DataTypes.STRING,
-
-        },
-        position: {
-            type: DataTypes.INTEGER,
-        },
-        imgUrl: {
-            type: DataTypes.STRING,
-        },
+  {
+    songId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
     },
-    
-    {
-        modelName: 'song',
-        sequelize: db,
-    }
-)
+    preview: {
+      type: DataTypes.STRING,
+    },
+    album: {
+      type: DataTypes.STRING,
+    },
+    artist: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    position: {
+      type: DataTypes.INTEGER,
+    },
+    imgUrl: {
+      type: DataTypes.STRING,
+    },
+  },
+
+  {
+    modelName: "song",
+    sequelize: db,
+  }
+);
 
 FriendList.init(
-    {
-        friendListId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement:true,
-            allowNull: false
-        },
+  {
+    friendListId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-        modelName: 'friendlist',
-        sequelize: db,
-    }
-)
+  },
+  {
+    modelName: "friendlist",
+    sequelize: db,
+  }
+);
 
 Friend.init(
-    {
-        friendId: {
-            type: DataTypes.INTEGER,
-            autoIncrement:true,
-            primaryKey: true,
-            allowNull: false,
-        },
+  {
+    friendId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-        modelName: 'friend',
-        sequelize: db,
-    }
-)
+  },
+  {
+    modelName: "friend",
+    sequelize: db,
+  }
+);
 
 Likes.init(
-{
+  {
     likesId: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-},
+  },
 
-{
-    modelName: 'likes',
+  {
+    modelName: "likes",
     sequelize: db,
-}
-)
-
+  }
+);
 
 // Playlist table
-Playlist.hasMany(Song, { foreignKey: 'playlistId'})
-Song.belongsTo(Playlist, {foreignKey: 'playlistId'})
+Playlist.hasMany(Song, { foreignKey: "playlistId" });
+Song.belongsTo(Playlist, { foreignKey: "playlistId" });
 
-User.hasMany(Playlist, {foreignKey:'userId'})
-Playlist.belongsTo(User, {foreignKey: 'userId'})
-
+User.hasMany(Playlist, { foreignKey: "userId" });
+Playlist.belongsTo(User, { foreignKey: "userId" });
 
 // Friendlist Table
-User.hasOne(FriendList, {foreignKey: 'userId'})
-FriendList.belongsTo(User, { foreignKey: 'userId'})
+User.hasOne(FriendList, { foreignKey: "userId" });
+FriendList.belongsTo(User, { foreignKey: "userId" });
 
-
-FriendList.hasMany(Friend, { foreignKey: 'friendListId'})
-Friend.belongsTo(FriendList, {foreignKey: 'friendListId'})
-
+FriendList.hasMany(Friend, { foreignKey: "friendListId" });
+Friend.belongsTo(FriendList, { foreignKey: "friendListId" });
 
 ///Friend table
-User.hasMany(Friend, { foreignKey: 'userId'})
-Friend.belongsTo(User, { foreignKey: 'userId' })
-
+User.hasMany(Friend, { foreignKey: "userId" });
+Friend.belongsTo(User, { foreignKey: "userId" });
 
 // Likes table
-User.hasMany(Likes, {foreignKey: 'userId'})
-Likes.belongsTo(User, {foreignKey: 'userId'})
+User.hasMany(Likes, { foreignKey: "userId" });
+Likes.belongsTo(User, { foreignKey: "userId" });
 
-
-Playlist.hasMany(Likes, { foreignKey: 'playlistId'})
-Likes.belongsTo(Playlist, {foreignKey: 'playlistId'})
-
-
-
+Playlist.hasMany(Likes, { foreignKey: "playlistId" });
+Likes.belongsTo(Playlist, { foreignKey: "playlistId" });
 
 // await db.sync({ force: true })
 // const testMixtape = await Playlist.create({ name: 'joe mix' })
--- src/Components/Friend/Friends.jsx
+++ src/Components/Friend/Friends.jsx
@@ -1,25 +1,32 @@
 import "../Friend/Friend.css";
 import LikePlaylist from "../LikedPlaylist/LikePlaylist";
 
-
-const Friends = ({ friend, index, setPageState, setFriendId, setLoadingState }) => {
-  console.log(friend)
+const Friends = ({
+  friend,
+  index,
+  setPageState,
+  setFriendId,
+  setLoadingState,
+}) => {
+  console.log(friend);
   return (
     <div>
-      <div id='friendsWrapper'>
+      <div id="friendsWrapper">
         <div className="friendsContainer" key={index}>
           <div
             onClick={() => {
               setFriendId(friend.userId);
-              setLoadingState("friendsPlayList")
+              setLoadingState("friendsPlayList");
               setPageState("loading");
             }}
             className="friendDisplay"
           >
             {friend.username}
           </div>
+          
           <div className="userId">{friend.userId}</div>
         </div>
+        <button className="follow-button">+ Follow</button>
       </div>
     </div>
   );
